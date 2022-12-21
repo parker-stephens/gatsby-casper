e4899e067075643c4ef947dda5e893d624bfc943
Parker Stephens <parker@parkercs.tech>
2022-12-20 19:33:48 -0600

    testing

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Casper',
-    description: 'A port of the casper blog built for gatsby',
-    siteUrl: 'https://gatsby-casper.netlify.com', // full path to blog - no ending slash
+    title: "Parker's Blog",
+    description: 'Your local technology blog with information from networking to hacking software on hardware.',
+    siteUrl: 'https://parkercs.tech', // full path to blog - no ending slash
   },
   mapping: {
     'MarkdownRemark.frontmatter.author': 'AuthorYaml.name',
